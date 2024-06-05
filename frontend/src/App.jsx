91ef3199f5a5296a2f59c7c709f7a25e74634c70
zainashraf259 <zainashraf2259@gmail.com>
2024-06-05 15:13:58 +0500

    FrontEnd Commincating with APIs (backend)

---
+++ frontend/src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// Pages & Components
+import Home from "./pages/Home";
+import Navbar from './components/Navbar'
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+      <Navbar/>
+        <div className="pages">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+
+import WorkoutDetails from '../components/WorkoutDetails'
+
+const Home = () => {
+  const [workouts, setWorkouts] = useState(null);
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const response = await fetch("/api/workouts");
+      const json = await response.json();
+
+      if (response.ok) {
+        setWorkouts(json);
+      }
+    };
+
+    fetchWorkouts();
+  }, []);
+
+  return (
+    <>
+      <div className="home">
+        <div className="workouts">
+          {/* {workouts &&
+            workouts.map((workout) => <p key={workout._id}>{workout.title}</p>)} */}
+            {workouts &&
+            workouts.map((workout) => <WorkoutDetails key={workout._id} workout={workout}/>)}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
